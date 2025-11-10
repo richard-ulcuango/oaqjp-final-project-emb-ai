f08d92a99d98ccb9a6d3b22146ba03618aa8b4a1
Richard <richardulcuango20@gmail.com>
2025-11-09 19:49:06 -0500

    Add emotion detection endpoint to Flask server

--- server.py
+++ server.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
 
 
 app = Flask(__name__)
-
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    # Extract the label and score from the response
+    label = response['label']
+    score = response['score']
+
+    # Return a formatted string with the sentiment label and score
+    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 
 if __name__ == "__main__":
     app.run(debug=True)
